23a92582899f69d24517513db8ecf5f7685957a5
satindra75 <39819518+satindra75@users.noreply.github.com>
2023-05-09 12:09:51 -0700

    Update Program.cs
    Added a compile error code.

--- automatedcodereview/automatedcodereview/Program.cs
+++ automatedcodereview/automatedcodereview/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hi, Automated code flow with the help of ChatGpt");
+Console.WriteLine("Automated code flow with the help of ChatGpt");
+Console.WriteLine("Checking the error!")

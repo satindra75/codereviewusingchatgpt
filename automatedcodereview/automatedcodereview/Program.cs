6aa13f563622fd8893b09ece5cac3790c58e4c15
satindra75 <39819518+satindra75@users.noreply.github.com>
2023-05-09 11:21:00 -0700

    Added a console writeline statement
    Added a console WriteLine statement.

--- automatedcodereview/automatedcodereview/Program.cs
+++ automatedcodereview/automatedcodereview/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hi, Automated code flow with the help of ChatGpt");
+Console.WriteLine("Automated code flow with the help of ChatGpt");
+Console.WriteLine("Validated the automated code flow with the help of ChatGpt");

e3f6ce2c8406531f032be751d450d6fc6582aad7
satindra75 <39819518+satindra75@users.noreply.github.com>
2023-05-10 10:50:55 -0700

    Create test.cs

---
+++ test.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.DesignPattern
+{
+  interface IQueue<T>
+  {
+     void push(T val);
+     T pop();
+     T peekAtFirst();
+     T peekAtLast();
+  }
+}

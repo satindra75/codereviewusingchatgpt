2d45d50be7cde5858a27b16c73b3f241a74d0c41
satindra75 <39819518+satindra75@users.noreply.github.com>
2023-05-10 10:08:53 -0700

    Create test.cs

---
+++ test.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.DesignPattern
+{
+    interface IQueue<T>
+    {
+          void push(T val);
+          T pop();
+          T peekAtFirst();
+          T peekAtLast();
+    }
+
+    class ObjectAdapter<T> : IQueue<T>
+    {
+        List<T> adaptee = new List<T>();
+        public void push(T val)
+        {
+            adaptee.Insert(0, val);
+        }
+
+        public T pop()
+        {
+            T val = adaptee[adaptee.Count - 1];
+            adaptee.RemoveAt(adaptee.Count - 1);
+            return val;
+        }
+
+        public T peekAtFirst()
+        {
+            return adaptee[0];
+        }
+
+        public T peekAtLast()
+        {
+            return adaptee[adaptee.Count - 1];
+        }
+    }
+
+    class ClassAdapter<T> : List<T>, IQueue<T>
+    {
+        public void push(T val)
+        {
+            push(val);
+        }
+
+        public T pop()
+        {
+            T val = this[Count - 1];
+            RemoveAt(Count - 1);
+            return val;
+        }
+
+        public T peekAtFirst()
+        {
+            return this[0];
+        }
+
+        public T peekAtLast()
+        {
+            return this[Count - 1];
+        }
+    }
+}
